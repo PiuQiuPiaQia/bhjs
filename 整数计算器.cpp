61a13ef9d5c8249be80f70711f7eb424c44ed307
Allen <329988300@qq.com>
2023-03-04 08:58:05 +0800

    init

---
+++ 整数计算器.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int op, a, b;
+	scanf("%d %d %d", &op, &a, &b);
+	long long ans;
+	if (op == 1) {
+		ans = a + b;
+	} else if (op == 2) {
+		ans = a - b;
+	} else if (op == 3) {
+		ans = a * b;
+	} else if (op == 4) {
+		ans = a / b;
+	}
+
+	if (ans != 1e18) {
+		printf("%d", ans);
+	} else {
+		printf("DIV 0");
+	}
+	return 0;
+}
