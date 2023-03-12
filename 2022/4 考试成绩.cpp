789f3dffb6007af939029fefd6a3fe86ec3e4a1b
Allen <329988300@qq.com>
2023-03-12 23:47:31 +0800

    update

--- 2022/4 考试成绩.cpp
+++ 2022/4 考试成绩.cpp
@@ -1,26 +1,61 @@
 #include <stdio.h>
 
-struct Student {
+typedef struct  {
 	int id;
 	char name[20];
 	double score;
-};
+} Student;
 
+
+Student sort(Student stus[], int n) {
+	Student max;
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (stus[j].score < stus[j + 1].score) {
+				max = stus[j];
+				stus[j] = stus[j + 1];
+				stus[j + 1] = max;
+			} else if (stus[j].score == stus[j + 1].score) {
+				if (stus[j].name < stus[j + 1].name) {
+					max = stus[j];
+					stus[j] = stus[j + 1];
+					stus[j + 1] = max;
+				} else if (stus[j].name == stus[j + 1].name) {
+					if (stus[j].id < stus[j + 1].id) {
+						max = stus[j];
+						stus[j] = stus[j + 1];
+						stus[j + 1] = max;
+					}
+				}
+			}
+		}
+	}
+	return *stus;
+}
 int main() {
 	int N;
 	scanf("%d", &N);
-	struct Student stus[N];
+	Student stus[N];
 	for (int i = 0; i < N; i++) {
 		scanf("%d %s %lf", &stus[i].id, &stus[i].name, &stus[i].score);
 	}
 
+	*stus = sort(stus, N);
 
 	for (int i = 0; i < N; i++) {
-		printf("%d %s %.2f", stus[i].id, stus[i].name, stus[i].score);
+		printf("%d %d %s %.2f\n", i, stus[i].id, stus[i].name, stus[i].score);
 	}
-	
-	
-	
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
