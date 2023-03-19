e53cd9e74d08bc3ea267ad416f20c226bbbf7a98
PiuQiuPiaQia <329988300@qq.com>
2023-03-19 01:39:26 +0000

    update

---
+++ 练习题/1135.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int sum = 0, a, n;
+  int x;
+  scanf("%d %d", &a, &n);
+  int temp = 0;
+  double b = 0.1;
+  for (int i = 1; i <= n; i++) {
+    b *= 10;
+    temp += a * b;
+    sum += temp;
+    // printf("%d\n", temp);
+  }
+  printf("%d\n", sum);
+  return 0;
+}
